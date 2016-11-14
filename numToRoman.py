2642557ca8bf643ea5f8d2348259f80913118630
Bryce <dukster98@hotmail.com>
2016-11-14 15:34:20 +1000

    Fix capital letters on valid = False Fix whitespace formatting errors from sublime Increased commenting

--- numToRoman.py
+++ numToRoman.py
@@ -11,13 +11,16 @@ General program structure:
 
 # Input string
 inputString = raw_input("Number to convert: ")
-print "Input string: " , inputString
+print "Input string : " , inputString
 
-valid = false
-while valid == false:
-    try:
+# While loop to check the validity of the input string
+valid = False
+while valid is False:
+    try:  # Assuming the input can be converted to an integer
         inNum = int(inputString)
-        valid = true
-    except Exception as e:
+        valid = True
+    except Exception as e:  # If the input can't be converted
         print "Invalid Input"
+        # Ask the user to input a valid string again
         inputString = raw_input("Number to convert: ")
+print "A valid input is now sitting in inNum"
