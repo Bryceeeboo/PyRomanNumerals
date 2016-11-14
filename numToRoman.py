aaab2e7dc5fd7fdd9798b3b09417d0ebf260306a
Bryce <dukster98@hotmail.com>
2016-11-14 15:08:20 +1000

    Initial commit Add input for number string

---
+++ numToRoman.py
@@ -0,0 +1,13 @@
+# Decimal number to roman numeral conversion
+
+"""
+General program structure:
+    Take a number as an input string
+    Check validity of input string
+    If string is valid
+        Convert number to roman numeral string
+    Output roman numeral string
+"""
+
+inputString = raw_input("Number to convert: ")
+print inputString
